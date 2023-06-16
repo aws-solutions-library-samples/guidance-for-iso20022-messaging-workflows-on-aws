7a16d76682d430128cba96029c7b9945fdc212c9
Eugene Istrati <eistrati@amazon.com>
2023-06-16 08:02:30 -0400

    minor bugfix

--- iac/s3_health/main.tf
+++ iac/s3_health/main.tf
@@ -48,6 +48,7 @@ resource "aws_s3_bucket_acl" "this" {
 }
 
 resource "aws_s3_bucket_policy" "this" {
+  count  = var.q.block_access ? 0 : 1
   bucket = aws_s3_bucket.this.id
   policy = data.aws_iam_policy_document.this.json
 

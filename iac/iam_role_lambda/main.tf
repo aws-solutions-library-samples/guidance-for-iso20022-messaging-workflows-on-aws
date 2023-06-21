e0f90d517d3d5d391e0b644bd62efa60b4455293
Eugene Istrati <eistrati@amazon.com>
2023-06-21 19:04:16 -0400

    removing managed policies from codebase

--- iac/iam_role_lambda/main.tf
+++ iac/iam_role_lambda/main.tf
@@ -3,7 +3,18 @@ resource "aws_iam_role" "this" {
   name               = format("%s-%s", var.q.name, data.terraform_remote_state.s3.outputs.rp2_id)
   description        = var.q.description
   path               = var.q.path
-  assume_role_policy = data.aws_iam_policy_document.this.json
+  assume_role_policy = data.aws_iam_policy_document.role.json
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_iam_policy" "this" {
+  count       = data.aws_region.this.name == element(keys(var.backend_bucket), 0) ? 1 : 0
+  name        = format("%s-%s", var.q.name, data.terraform_remote_state.s3.outputs.rp2_id)
+  description = var.q.description
+  policy      = data.aws_iam_policy_document.policy.json
 
   lifecycle {
     create_before_destroy = true
@@ -11,7 +22,7 @@ resource "aws_iam_role" "this" {
 }
 
 resource "aws_iam_role_policy_attachment" "this" {
-  count      = data.aws_region.this.name == element(keys(var.backend_bucket), 0) ? length(local.policy_arns) : 0
+  count      = data.aws_region.this.name == element(keys(var.backend_bucket), 0) ? 1 : 0
   role       = element(aws_iam_role.this.*.name, 0)
-  policy_arn = element(local.policy_arns, count.index)
+  policy_arn = element(aws_iam_policy.this.*.arn, 0)
 }

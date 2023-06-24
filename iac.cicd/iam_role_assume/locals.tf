8729baa41f76604f2dff092700265c56f0176558
Eugene Istrati <eistrati@amazon.com>
2023-06-24 18:55:06 -0400

    minor improvements

--- iac.cicd/iam_role_assume/locals.tf
+++ iac.cicd/iam_role_assume/locals.tf
@@ -1,12 +1,12 @@
 locals {
   ips = {
-    for val in jsondecode(data.http.this.response_body)["prefixes"]: val["service"] => val... if (
-      val["service"] == "CODEBUILD" && (
+    for val in jsondecode(data.http.this.response_body)["prefixes"]: lower(val["service"]) => val... if (
+      lower(val["service"]) == "codebuild" && (
         val["region"] == element(keys(var.backend_bucket), 0)
         || val["region"] == element(keys(var.backend_bucket), 1)
       )
     )
   }
-  policy_ips  = local.ips["CODEBUILD"].*.ip_prefix
+  policy_ips  = local.ips["codebuild"].*.ip_prefix
   policy_arns = ["arn:aws:iam::aws:policy/AdministratorAccess"]
 }

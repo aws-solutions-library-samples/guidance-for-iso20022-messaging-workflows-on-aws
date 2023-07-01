0442e6ee91a53dc582dad01aad1121e1dbff3b04
Eugene Istrati <eistrati@amazon.com>
2023-07-01 12:45:43 -0400

    iam roles and policies refactoring

--- iac.src/lambda_health/locals.tf
+++ iac.src/lambda_health/locals.tf
@@ -1,4 +1,8 @@
 locals {
-  rp2_id = data.terraform_remote_state.s3.outputs.rp2_id
   domain = format("%s.%s", data.aws_region.this.name, data.terraform_remote_state.s3.outputs.custom_domain)
+  rp2_id = data.terraform_remote_state.s3.outputs.rp2_id
+  region = (
+    data.aws_region.this.name == element(keys(var.backend_bucket), 0)
+    ? element(keys(var.backend_bucket), 1) : element(keys(var.backend_bucket), 0)
+  )
 }
